eb16683cd87289a9b830bb041a6d2aadbf4d7920
dgotlieb <Dgotlieb8@gmail.com>
2020-08-04 20:14:13 +0300

    Create flask_test.py

---
+++ flask_test.py
@@ -0,0 +1,20 @@
+from random import randint
+from flask import Flask
+
+app = Flask(__name__)
+
+# accessed via <HOST>:<PORT>/get_random
+@app.route("/get_random")
+def random():
+    random_number = randint(1, 10)
+    return {'random': random_number}
+
+# accessed via <HOST>:<PORT>/welcome
+@app.route("/welcome")
+def welcome():
+    return "<H1 id='welcome'>Welcome!</H1>"
+
+# host is pointing at local machine address
+# debug is used for more detailed logs + hot swaping
+# the desired port - feel free to change
+app.run(host='127.0.0.1', debug=True, port=5000)
