f2752e17497bd402e729c6fcefd26998afce9d2c
dgotlieb <Dgotlieb8@gmail.com>
2020-08-08 19:11:06 +0300

    Create rest_test.py

---
+++ rest_test.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# supported methods 
+@app.route('/data/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def user(user_id):
+    if request.method == 'GET':
+        return 'Hello ' + user_id
+
+    elif request.method == 'POST':
+        # getting the json data payload from request
+        request_data = request.json
+        # treating request_data as a dictionary to get a specific value from key
+        user_id = request_data.get('user_id')
+        return 'Great ' + user_id
+  # todo elif for put and delete
+
+
+app.run(host='127.0.0.1', debug=True, port=5000)
