e9ac29bcb9406e7e3fe95c4896ae443a451f9f13
dgotlieb <Dgotlieb8@gmail.com>
2020-08-13 21:24:40 +0300

    Update flask_web_test.py

--- flask_web_test.py
+++ flask_web_test.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 @app.route("/get_random")
 def random():
     random_number = randint(1, 10)
-    return {'random': random_number}
+    return {'random': random_number}, 200 # status code
 
 # using default
 @app.route('/hello')
 @app.route('/hello/<user_name>')
 def hello_user(user_name = 'no one'):
-    return 'Hello ' + user_name
+    return 'Hello ' + user_name, 200 # status code
 
 
 # accessed via <HOST>:<PORT>/welcome
 @app.route("/welcome")
 def welcome():
-    return "<H1 id='welcome'>Welcome!</H1>"
+    return "<H1 id='welcome'>Welcome!</H1>", 200 # status code
 
 # host is pointing at local machine address
 # debug is used for more detailed logs + hot swaping
